9de2c10d1cdc7738e0dd64d32a7c06836067a1e0
Yisus <jesu_ti91@hotmail.com>
2025-02-25 12:11:36 -0500

    Create BusinessLogicApplicationTests.java

---
+++ business-logic/src/test/java/co/com/jdti/businesslogic/BusinessLogicApplicationTests.java
@@ -0,0 +1,12 @@
+package co.com.jdti.businesslogic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessLogicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
