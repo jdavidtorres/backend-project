ad63aa627b6018ae23c8c5f028dcf020b579a9da
Yisus <jesu_ti91@hotmail.com>
2025-02-25 23:27:16 -0500

    Update JwtAuthenticationFilter.java

--- core-security/src/main/java/co/com/jdti/coresecurity/filter/JwtAuthenticationFilter.java
+++ core-security/src/main/java/co/com/jdti/coresecurity/filter/JwtAuthenticationFilter.java
@@ -43,7 +43,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 				UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
 				authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 				SecurityContextHolder.getContext().setAuthentication(authentication);
-				log.debug("Authenticated user {}", username);
+				log.debug("User {} authenticated", username);
 			} else {
 				log.error("Invalid token");
 			}
