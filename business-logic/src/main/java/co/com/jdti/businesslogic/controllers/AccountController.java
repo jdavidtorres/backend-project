d216e6dd1e89f2ae6ea656e1dc91b701be3f1b26
Yisus <jesu_ti91@hotmail.com>
2025-02-25 23:25:58 -0500

    Remove authentication annotation from getMyAccounts method in AccountController

--- business-logic/src/main/java/co/com/jdti/businesslogic/controllers/AccountController.java
+++ business-logic/src/main/java/co/com/jdti/businesslogic/controllers/AccountController.java
@@ -5,7 +5,6 @@ import co.com.jdti.businesslogic.services.IAccountService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +19,6 @@ public class AccountController {
 
 	private final IAccountService accountService;
 
-	@PreAuthorize("isAuthenticated()")
 	@GetMapping
 	public ResponseEntity<List<AccountDTO>> getMyAccounts() {
 		List<AccountDTO> accounts = accountService.getMyAccounts();
